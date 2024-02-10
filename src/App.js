99b96afc9f6c67088e2366f69c8871e1cda9e081
snehakss <snehakallingal123@gmail.com>
2024-02-10 14:55:24 +0530

    view is added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import Viewpost from './components/Viewpost';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<SignIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/view' element={<Viewpost/>}/>
       </Routes>
       </BrowserRouter>
     </div>
